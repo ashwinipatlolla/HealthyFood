914d917df82ea24d233ca639fc154fd85c28c12e
ashwini <w9601599@live.tees.ac.uk>
2023-11-28 16:45:47 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/mad/w9601599/MainActivity.kt
@@ -0,0 +1,23 @@
+package uk.ac.tees.mad.w9601599
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if(FirebaseAuth.getInstance().currentUser==null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
