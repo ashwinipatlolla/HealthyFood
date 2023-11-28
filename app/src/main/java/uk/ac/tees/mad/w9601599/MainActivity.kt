304eb004d80426482393c2aab9ee241ce5062a00
ashwini <w9601599@live.tees.ac.uk>
2023-11-28 16:46:28 +0000

    hello

--- app/src/main/java/uk/ac/tees/mad/w9601599/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/w9601599/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-
+//hello
     override fun onStart() {
         super.onStart()
         if(FirebaseAuth.getInstance().currentUser==null){
